5d83ae62870182c72802509a4aa8643162a5a322
Ujjawal Singh <86152228+ayeujjawalsingh@users.noreply.github.com>
2022-09-24 05:49:59 -0700

    Update 100_Code.java

--- PepInsta_100_Code/100_Code.java
+++ PepInsta_100_Code/100_Code.java
@@ -580,3 +580,23 @@ public class Main
         return sum;
     }
 }
+
+// 28th Highest Common Factors(HCF)
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		int hcf = 0;
+		for(int i = 1;i<=num1 || i<= num2;i++){
+		    if(num1%i == 0 && num2%i == 0){
+		        hcf = i;
+		    }
+		}
+		System.out.println(hcf);
+		
+	}
+}
