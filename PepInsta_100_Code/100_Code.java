37045e67915c630ca1455b5c8c0bc17e41f20297
Ujjawal Singh <86152228+ayeujjawalsingh@users.noreply.github.com>
2022-09-24 06:05:04 -0700

    Added LCM Code

--- PepInsta_100_Code/100_Code.java
+++ PepInsta_100_Code/100_Code.java
@@ -600,3 +600,50 @@ public class Main
 		
 	}
 }
+
+
+// 30th Lowest Common Multiple (LCM)
+
+// Method 1
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		int lcm = 0;
+		int max = Math.max(num1,num2);
+		for(int i = max;i<=num1*num2;i++){
+		    if(i%num1 == 0 && i%num2 == 0){
+		        lcm = i;
+		        break;
+		    }
+		}
+		System.out.println(lcm);
+		
+	}
+}
+
+// Method 2
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		int hcf = 0;
+		for(int i = 1;i<=num1 || i<= num2;i++){
+		    if(num1%i == 0 && num2%i == 0){
+		        hcf = i;
+		    }
+		}
+		int lcm = (num1*num2)/hcf;
+		System.out.println(lcm);
+		
+	}
+}
+
