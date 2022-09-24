beec88ee775c887061fe47aebacfc49f45efdd1e
Ujjawal Singh <ayeujjawalsingh@gmail.com>
2022-09-24 14:12:51 +0530

    Added Sorting Method

---
+++ Sorting_Algorithm/SortType.java
@@ -0,0 +1,16 @@
+
+public enum SortType {
+    BUBBLE,
+    BITONIC,
+    HEAP,
+    INSERTION,
+    MERGE,
+    QUICK,
+    SELECTION,
+    CYCLE,
+    SHELL,
+    INTRO,
+    COMB,
+    GNOME
+}
+
