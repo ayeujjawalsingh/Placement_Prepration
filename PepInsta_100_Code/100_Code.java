dd56547f9d9539936bf05f92e96d9373aa48d5a4
Ujjawal Singh <86152228+ayeujjawalsingh@users.noreply.github.com>
2022-09-24 06:56:20 -0700

    Hexadecimal to Decimal conversion

--- PepInsta_100_Code/100_Code.java
+++ PepInsta_100_Code/100_Code.java
@@ -722,4 +722,22 @@ public class Main
 	}
 }
 
+// 33th Hexadecimal to Decimal conversion
 
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String hex = sc.next();
+		String digits = "0123456789ABCDEF";  
+		hex = hex.toUpperCase();  
+		int val = 0;  
+		for (int i = 0; i < hex.length(); i++)  {  
+		    char c = hex.charAt(i);  
+		    int d = digits.indexOf(c);  
+		    val = 16*val + d;  
+		}  
+		System.out.print(val);
+	}
+}
