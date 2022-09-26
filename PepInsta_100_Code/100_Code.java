9a2521c032b822bf91c46aa35cce95f61de0264e
Ujjawal Singh <86152228+ayeujjawalsingh@users.noreply.github.com>
2022-09-26 07:19:13 -0700

    Added Addition of two fraction

--- PepInsta_100_Code/100_Code.java
+++ PepInsta_100_Code/100_Code.java
@@ -1,4 +1,4 @@
-//First Code Even And Odd
+//1st Code Even And Odd
 
 import java.util.*;
 public class Main
@@ -937,5 +937,62 @@ public class Main
 		int num = sc.nextInt();
 		int total = num * (num-1) / 2;
 		System.out.println("For "+ num +" people there will be " +total+" handshakes");
+	}
+}
+
+// 43th Addition of two fractions
+
+import java.util.*;
+public class Main{
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		//input from the user		
+		System.out.print("Enter numerator for first fraction : ");		
+		int num1 = sc.nextInt();
+		System.out.print("Enter denominator for first fraction : ");		
+		int den1 = sc.nextInt();
+		System.out.print("Enter numerator for second fraction : ");		
+		int num2 = sc.nextInt();
+		System.out.print("Enter denominator for second fraction : ");		
+		int den2 = sc.nextInt();
+		int num, den, x;
+		System.out.print("("+num1+" / "+den1+") + ("+num2+" / "+den2+") = ");
+		if(den1 == den2)
+		{
+			num = num1 + num2 ;
+			den = den1 ;
+		}
+		else{
+			num = (num1*den2) + (num2*den1);
+			den = den1 * den2;
+		}
+		if(num > den)
+			x = num;
+		else
+			x = den;
+		for(int i = 1 ; i <= x ; i++)
+		{
+			if(num%i == 0 && den%i == 0)
+			{
+				num = num/i;
+				den = den/i;
+			}
+		}
+		int n = 1;
+		int p = num;
+		int q = den;
+		if( num != den)
+		{
+			while(n != 0)
+			{
+				n = num % den;			
+				if(n != 0)
+				{
+					num = den;
+					den = n;
+				}
+			}			
+		}
+		System.out.println("("+p/den+" / "+q/den+")");
 	}
 }
