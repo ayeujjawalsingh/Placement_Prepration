d05f2e15109e63eda3fd3c2d5db9dc1b2879d280
Ujjawal Singh <86152228+ayeujjawalsingh@users.noreply.github.com>
2022-09-29 11:14:26 -0700

    Added 45th Number of code

--- PepInsta_100_Code/100_Code.java
+++ PepInsta_100_Code/100_Code.java
@@ -1019,3 +1019,42 @@ public class Main
 		System.out.println("Converted number is: "+str1);
 	}
 }
+
+// 45th Can a number be expressed as a sum of two prime numbers
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number : ");				
+		int number = sc.nextInt();
+		int x = 0;
+		for(int i = 2 ; i <= number/2 ; i++)
+		{
+			if(prime_or_not(i) == 1)
+			{
+				if(prime_or_not(number-i) == 1)
+				{
+					System.out.println(number+ " = "+i+" + "+(number-i));
+					x = 1;
+				}
+			}
+		}
+		if(x == 0)
+			System.out.println(+number+" cannot be expressed as a sum of two prime numbers");
+	}
+	
+	public static int prime_or_not(int n){
+		int c = 1;
+		for(int i = 2 ; i < n ; i++)
+		{
+			if(n % i == 0)
+			{
+				c = 0;
+				break;
+			}
+		}
+		return c;
+	}
+}
