1d4815dbb97d96f7773c25e0f413b484d2a87248
Ujjawal Singh <86152228+ayeujjawalsingh@users.noreply.github.com>
2022-09-30 11:51:26 -0700

    Finding the Roots of a quadratic equation

--- PrepInsta_100_Code/100_Code.java
+++ PrepInsta_100_Code/100_Code.java
@@ -1168,3 +1168,44 @@ class Main{
 
 	}
 }
+
+// 55th
+
+import java.util.*;
+import static java.lang.Math.*;
+class Main{
+ 
+    static void findRoots(int a, int b, int c)
+    {
+        if (a == 0) {
+            System.out.println("Invalid");
+            return;
+        }
+ 
+        int d = b * b - 4 * a * c;
+        double sqrt_val = sqrt(abs(d));
+ 
+        if (d > 0) {
+            System.out.println("Roots are real and different");
+            System.out.println((double)(-b + sqrt_val) / (2 * a) + "\n"+ (double)(-b - sqrt_val) / (2 * a));
+        }
+        else if (d == 0) {
+            System.out.println("Roots are real and same ");
+            System.out.println(-(double)b / (2 * a) + "\n" + -(double)b / (2 * a));
+        }
+        else // d < 0
+        {
+            System.out.println("Roots are complex");
+ 
+            System.out.println(-(double)b / (2 * a) + " + i" + sqrt_val + "\n" + -(double)b / (2 * a) + " - i" + sqrt_val);
+        }
+    }
+    public static void main(String args[])
+    {
+ 	Scanner sc = new Scanner(Syatem.in);
+        int a = sc.nextInt();
+	int b = sc.nextInt();
+	int c = sc.nextInt();
+        findRoots(a, b, c);
+    }
+}
