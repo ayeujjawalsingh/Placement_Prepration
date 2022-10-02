faf00d6251288f0115bb26679b3cd23d35234496
Ujjawal Singh <86152228+ayeujjawalsingh@users.noreply.github.com>
2022-10-02 07:35:24 -0700

    count no.  of integer which has exactly x divisor

--- PrepInsta_100_Code/100_Code.java
+++ PrepInsta_100_Code/100_Code.java
@@ -1169,6 +1169,31 @@ class Main{
 	}
 }
 
+// 53th Write a program to count Number of integers which has exactly X divisors 
+
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int  n = sc.nextInt();
+        int x = sc.nextInt();
+        int count = 0;
+        for(int i=1; i<=n; i++){
+            //variable to count the factors of i-th number
+            int count_factors = 0;
+            for(int j = 1; j<= i; j++){
+                if(i%j==0){
+                    count_factors++;
+                }
+            }
+        
+            if(count_factors == x)
+                count++;
+        }
+        System.out.println(count);
+    }
+}
+
 // 54th Finding Roots of a quadratic equation
 
 import java.util.*;
