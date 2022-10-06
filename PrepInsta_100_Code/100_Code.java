ac1bafe642e65eb7cfd16559454d5e62f791c419
Ujjawal Singh <86152228+ayeujjawalsingh@users.noreply.github.com>
2022-10-06 10:50:33 -0700

    Update 100_Code.java

--- PrepInsta_100_Code/100_Code.java
+++ PrepInsta_100_Code/100_Code.java
@@ -1562,3 +1562,30 @@ class Main{
         Reversing(n/10,len,++i);
     }
 }
+
+// 60th HCF of two numbers using recursion
+
+import java.util.*;
+class Main{
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        int num1 =  sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println(gcd(num1,num2));
+    }
+    public static int gcd(int num1, int num2){
+        if (num1 == 0)
+          return num2;
+          
+        if (num2 == 0)
+          return num1;
+
+        // base case
+        if (num1 == num2)
+            return num1;
+            
+        if (num1 > num2)
+            return gcd(num1-num2, num2);
+        return gcd(num1, num2-num1);
+    }
+}
