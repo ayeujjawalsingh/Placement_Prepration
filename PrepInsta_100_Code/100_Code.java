a577b7e17a11cd465494dba2ec4c80adb7b2e9b2
Ujjawal Singh <86152228+ayeujjawalsingh@users.noreply.github.com>
2022-10-09 09:41:01 -0700

    Update 100_Code.java

--- PrepInsta_100_Code/100_Code.java
+++ PrepInsta_100_Code/100_Code.java
@@ -1635,3 +1635,28 @@ class Main{
     }
     
 }
+
+// 63th Print All Permutations of a String using recursion
+
+import java.util.*;
+class Main {
+    static void printPermutn(String str, String ans) {
+        if (str.length() == 0) {
+            System.out.print(ans + " ");
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            String r = str.substring(0, i) + str.substring(i + 1);
+
+            printPermutn(r, ans + ch);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s =  sc.next();
+        printPermutn(s, "");
+    }
+}
