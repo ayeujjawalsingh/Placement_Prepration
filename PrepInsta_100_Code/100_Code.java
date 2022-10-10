ec44ab026e28199ce9a96c686f05209284be0b9a
Ujjawal Singh <86152228+ayeujjawalsingh@users.noreply.github.com>
2022-10-10 03:40:50 -0700

    Update 100_Code.java

--- PrepInsta_100_Code/100_Code.java
+++ PrepInsta_100_Code/100_Code.java
@@ -1660,3 +1660,23 @@ class Main {
         printPermutn(s, "");
     }
 }
+
+// 64th Given an integer N the task is to print the F(N)th term using recursion
+
+import java.util.*;
+class Main {
+    static int term (int calculated, int current, int N){
+        int i, cur = 1;
+        if (current == N + 1)
+            return 0;
+        for (i = calculated; i < calculated + current; i++)
+            cur *= i;
+        return cur + term (i, current + 1, N);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =  sc.nextInt();
+        System.out.println (term (1, 1, n));
+    }
+}
+
