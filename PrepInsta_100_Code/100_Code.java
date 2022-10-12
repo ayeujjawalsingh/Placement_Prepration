c17ca65787537c0f8fc92750ff5ac49bb27c0500
Ujjawal Singh <86152228+ayeujjawalsingh@users.noreply.github.com>
2022-10-12 10:02:47 -0700

    Added non-zero digit in factorial using recursion

--- PrepInsta_100_Code/100_Code.java
+++ PrepInsta_100_Code/100_Code.java
@@ -1702,3 +1702,25 @@ public class Main{
         subsetSum(arr, l+1, r, sum);
     }
 }
+
+// 66th Last non-zero digit in factorial using recursion
+
+import java.util.*;
+public class Main{
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int fact = factorial(n);
+        while(fact%10==0){
+            fact/=10;
+        }
+        System.out.print(fact%10);
+    }
+    
+    private static int factorial(int n){
+        if(n==0 || n == 1){
+            return 1;
+        }
+        return n * factorial(n-1);
+    }
+}
