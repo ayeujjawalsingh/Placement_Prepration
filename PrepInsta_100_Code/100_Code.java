16de8bbd496fe4b62a3cbdf4ad718c334d160e3e
Ujjawal Singh <86152228+ayeujjawalsingh@users.noreply.github.com>
2022-10-15 23:37:57 -0700

    Palindrme Number

--- PrepInsta_100_Code/100_Code.java
+++ PrepInsta_100_Code/100_Code.java
@@ -1806,3 +1806,66 @@ public class Main{
         return n * factorial(n - 1);
     }
 }
+
+// 70th Find all possible Palindromic partitions of the given String
+
+import java.util.*;
+public class Main{
+    public static void main(String []args){
+        Scanner sc =new Scanner(System.in);
+        String input = sc.next();
+        System.out.println("All possible palindrome partitions for " + input+ " are :");
+        allPalPartitions(input);
+    }
+    private static void allPalPartitions(String input)
+    {
+        int n = input.length();
+ 
+        ArrayList<ArrayList> allPart = new ArrayList<>();
+ 
+        Deque currPart = new LinkedList();
+ 
+        allPalPartitonsUtil(allPart, currPart, 0, n, input);
+ 
+        for (int i = 0; i < allPart.size(); i++)
+        {
+            for (int j = 0; j < allPart.get(i).size(); j++)
+            {
+                System.out.print(allPart.get(i).get(j) + " ");
+            }
+            System.out.println();
+        }
+ 
+    }
+ 
+    private static void allPalPartitonsUtil(ArrayList<ArrayList> allPart,
+    Deque currPart, int start, int n, String input)
+    {
+        if (start >= n)
+        {
+            allPart.add(new ArrayList<>(currPart));
+            return;
+        }
+ 
+        for (int i = start; i < n; i++)
+        {
+            if (isPalindrome(input, start, i))
+            {
+                currPart.addLast(input.substring(start, i + 1));
+                allPalPartitonsUtil(allPart, currPart, i + 1, n, input);
+                currPart.removeLast();
+            }
+        }
+    }
+ 
+    private static boolean isPalindrome(String input,int start, int i)
+    {
+        while (start < i)
+        {
+            if (input.charAt(start++) != input.charAt(i--))
+                return false;
+        }
+        return true;
+    }
+}
+
