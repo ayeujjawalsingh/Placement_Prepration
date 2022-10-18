aa630cf0b8cb2a5061212c9913552a510e610d21
Ujjawal Singh <86152228+ayeujjawalsingh@users.noreply.github.com>
2022-10-18 11:59:58 -0700

    Update 100_Code.java

--- PrepInsta_100_Code/100_Code.java
+++ PrepInsta_100_Code/100_Code.java
@@ -1899,3 +1899,54 @@ public class Main{
     }
 }
 
+// 72th Given a set of positive integers, find all its subsets using recursion
+
+import java.io.*;
+import java.util.*;
+class Main {
+    public static void findSubsets(List<List<Integer>> subset, ArrayList<Integer> nums, ArrayList<Integer> output, int index)
+    {
+        if (index == nums.size()) {
+            subset.add(output);
+            return;
+        }
+       
+        findSubsets(subset, nums, new ArrayList<>(output), index + 1);
+ 
+        output.add(nums.get(index));
+        findSubsets(subset, nums, new ArrayList<>(output), index + 1);
+    }
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int len = sc.nextInt();
+        List<List<Integer>> subset = new ArrayList<>();
+        ArrayList input = new ArrayList<>();
+        for(int i = 0;i<len;i++){
+            input.add(sc.nextInt());
+        }
+       
+        findSubsets(subset, input, new ArrayList<>(), 0);
+ 
+        Collections.sort(subset, (o1, o2) -> {
+            int n = Math.min(o1.size(), o2.size());
+            for (int i = 0; i < n; i++) {
+                if (o1.get(i) == o2.get(i)){
+                    continue;
+                }else{
+                    return o1.get(i) - o2.get(i);
+                }
+            }
+            return 1;
+        });
+       
+       
+        for(int i = 0; i < subset.size(); i++){
+            for(int j = 0; j < subset.get(i).size(); j++){
+                System.out.print(subset.get(i).get(j) + " ");
+            }
+            System.out.println();
+        }
+       
+    }
+}
